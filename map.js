98636121148e47ff3408df04d76e25957c610829
weeklyd3 <timothywei.spam@gmail.com>
2024-07-05 03:43:03 +0000

    make raw have less letters

--- map.js
+++ map.js
@@ -193,7 +193,7 @@ function nd(display) {
 	display.textSize(12);
 	display.text('map (C) OpenStreetMap contributors\nhttps://openstreetmap.org/copyright', 1920 / 4, 0);
 	display.textAlign('left', 'top');
-	display.text(`GPS:\n${lastLocation.coords.latitude}, ${lastLocation.coords.longitude}\nGS ${(lastLocation.coords.speed == null || lastLocation.coords.speed != lastLocation.coords.speed) ? lastLocation.coords.speed : lastLocation.coords.speed * 3600 / 1852} kts (raw: ${lastLocation.coords.speed} m/s)\nHDG ${lastLocation.coords.heading}\n${(lastLocation.coords.altitude == null || lastLocation.coords.altitude != lastLocation.coords.altitude) ? lastLocation.coords.altitude : Math.round(lastLocation.coords.altitude * 100 / 2.54 / 12)} ft amsl (raw: ${lastLocation.coords.altitude} m)\nf#${fcount}`, 0, 0)
+	display.text(`GPS:\n${lastLocation.coords.latitude}, ${lastLocation.coords.longitude}\nGS ${(lastLocation.coords.speed == null || lastLocation.coords.speed != lastLocation.coords.speed) ? lastLocation.coords.speed : lastLocation.coords.speed * 3600 / 1852} kts (raw: ${String(lastLocation.coords.speed).slice(0, 7)} m/s)\nHDG ${lastLocation.coords.heading}\n${(lastLocation.coords.altitude == null || lastLocation.coords.altitude != lastLocation.coords.altitude) ? lastLocation.coords.altitude : Math.round(lastLocation.coords.altitude * 100 / 2.54 / 12)} ft amsl (raw: ${String(lastLocation.coords.altitude).slice(0, 7)} m)\nf#${fcount}`, 0, 0)
 	if (player.geo_err) {
 		display.fill("red");
 		display.rect(1920 / 4 - 100, 30, 100, 30);
